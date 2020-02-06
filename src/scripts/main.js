c9ac3a78f961ea56cbc5a7f7fad92ed186281fb0
zaxwebs <zifidigital@gmail.com>
2020-02-07 01:12:30 +0530

    Initial work on contact section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,13 @@
 import $ from 'jquery'
 import 'bootstrap'
 import lax from 'lax.js'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 window.onload = function() {
+	AOS.init({
+		offset: 50
+	})
 	lax.setup() // init
 
 	const updateLax = () => {
